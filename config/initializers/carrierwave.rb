e546aa7502eaa80e1b8a3946c7d78103efe62864
zanderol <olexandruk@gmail.com>
2020-02-19 21:22:54 +0200

    carrierwave change region eu-west-2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,7 +17,7 @@ if Rails.env.production?
         :provider               => 'AWS',                        # required
         :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
         :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
-        :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+        :region                 => 'eu-west-2',                  # optional, defaults to 'us-east-1'
         :host                   => 's3.example.com',             # optional, defaults to nil
         :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
     }
