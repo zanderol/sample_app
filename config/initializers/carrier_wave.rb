a05c23ace18c88168bcb4702ac108e25af14ba32
ZanderOl <olexandruk@gmail.com>
2020-02-20 11:14:13 +0200

    Update carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,29 +1,27 @@
-if Rails.env.production?
-  # CarrierWave.configure do |config|
-  #   config.fog_credentials = {
-  #       # Settings for Amazon S3
-  #       :provider              => 'AWS',
-  #       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-  #       :aws_secret_access_key => ENV['S3_SECRET_KEY']
-  #   }
-  #   config.fog_directory     =  ENV['S3_BUCKET']
-  # end
+# if Rails.env.production?
+#   # CarrierWave.configure do |config|
+#   #   config.fog_credentials = {
+#   #       # Settings for Amazon S3
+#   #       :provider              => 'AWS',
+#   #       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+#   #       :aws_secret_access_key => ENV['S3_SECRET_KEY']
+#   #   }
+#   #   config.fog_directory     =  ENV['S3_BUCKET']
+#   # end
+#
+# end
 
-  CarrierWave.configure do |config|
-    config.root = Rails.root.join('tmp') # adding these...
-    config.cache_dir = 'carrierwave' # ...two lines
+CarrierWave.configure do |config|
+  config.root = Rails.root
+  config.storage :file
 
-    config.fog_credentials = {
-        :provider               => 'AWS',                        # required
-        :s3_access_key_id      => ENV['S3_ACCESS_KEY'],                        # required
-        :s3_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
-        :region                 => 'eu-west-2',                  # optional, defaults to 'us-east-1'
-        :host                   => 's3.example.com',             # optional, defaults to nil
-        :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
-    }
-    config.fog_directory  = ENV['S3_Bucket']                             # required
-    config.fog_public     = false                                   # optional, defaults to true
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  def store_dir
+    "#{Rails.root}/public/uploads"
   end
 
+  #I am not sure about this one ~SS
+  def cache_dir
+    "#{Rails.root}/public/uploads"
+  end
 end
+
